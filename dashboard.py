f077df1feb9ccc1f75c3efb72ffa3fea3c658c9d
Suhas Aitham <83656128+suhasaitham22@users.noreply.github.com>
2023-06-30 11:17:47 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,11 +11,32 @@ def load_data(sheets_url):
 df = load_data(st.secrets["public_gsheets_url"])
 
 st.set_page_config(
-    page_title = 'CAMINO LCSNA Real-Time Dashboard',
-    page_icon= '*',
+    page_title='CAMINO LCSNA Real-Time Dashboard',
+    page_icon='*',
     layout='wide'
 )
 
-st.title('Real-Time LCSNA Dahboard')
+# Sidebar navigation
+page = st.sidebar.selectbox('Page', ('Home', 'Gender Differences', 'Residency Differences'))
 
-st.write(df)
+# Home Page
+if page == 'Home':
+    st.title('Home')
+    st.header('About Camino Research Institute')
+    st.write('Add a brief description of the Camino Research Institute here.')
+    
+    st.header('Data Collection Methodology')
+    st.write('Describe the data collection methodology used by the Camino Research Institute here.')
+    
+    st.header('Team Members')
+    st.write('Introduce the team members of the Camino Research Institute here.')
+
+# Gender Differences Page
+elif page == 'Gender Differences':
+    st.title('Gender Differences')
+    # Add content specific to the Gender Differences page here
+    
+# Residency Differences Page
+elif page == 'Residency Differences':
+    st.title('Residency Differences')
+    # Add content specific to the Residency Differences page here
