a0886a75dfd96c151a024a53b69e075cf8457995
Suhas Aitham <83656128+suhasaitham22@users.noreply.github.com>
2023-06-30 11:02:35 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,16 +3,6 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import time
-# import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
-
-# def authenticate_google_sheets():
-#     scope = ['https://spreadsheets.google.com/feeds',
-#              'https://www.googleapis.com/auth/drive']
-#     creds = ServiceAccountCredentials.from_json_keyfile_name(
-#         'credentials.json', scope)
-#     client = gspread.authorize(creds)
-#     return client.open('dashboard export').sheet1  # Replace with your own sheet name
 
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
@@ -27,11 +17,5 @@ st.set_page_config(
 )
 
 st.title('Real-Time LCSNA Dahboard')
-
-county_filter = st.selectbox('Select a County', pd.unique(df['County']))
-
-placeholder=st.empty()
-
-df = df[df['County']==county_filter]
 
 st.write(df)
